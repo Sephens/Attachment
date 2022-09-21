bf05bbdf32cb46b834cdc2fa24c3d10158ec416d
Sephens <adesephens@gmail.com>
2022-09-21 14:25:09 +0300

    react-buget-app day 1

---
+++ budget-calculator/src/components/Alert.js
@@ -0,0 +1,8 @@
+import { type } from "@testing-library/user-event/dist/type";
+import React from "react";
+
+const Alert = ({ type, text }) => {
+    return <div className={`alert alert-${type}`}>{text}</div>;
+  };
+  
+  export default Alert;

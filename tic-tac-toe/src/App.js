8d31660aa9392aaa0536e517e8713fa91bb9254d
Sephens <adesephens@gmail.com>
2022-09-27 14:54:32 +0300

    building the game components with React

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,72 @@
+import React from "react"
+import './App.css';
+import { ReactDOM } from "react";
+
+//component One: Square
+// The Square component renders a single <button>
+class Square extends React.Component {
+    render() {
+      return (
+        <button className="square">
+          {this.props.value}
+        </button>
+      );
+    }
+  }
+  
+//component Two: Board
+// Board renders 9 squares
+  class Board extends React.Component {
+    renderSquare(i) {
+      return <Square value = {i} />;
+    }
+  
+    render() {
+      const status = 'Next player: X';
+  
+      return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+// component Three: Game
+// The Game component renders a board
+  class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  // ========================================
+  
+//   const root = ReactDOM.createRoot(document.getElementById("root"));
+//   root.render(<Game />);
+  export default Game;

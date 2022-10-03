ca64dc2d3aaee1888aada4715e4ca3058d9d5ac2
Sephens <adesephens@gmail.com>
2022-10-03 16:15:33 +0300

    week 3 day1

---
+++ task-manager/mongo-frontend-master/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CreateUser from "./components/createuser";
+
+function App() {
+  return (
+    <div className="app">
+      <CreateUser />
+    </div>
+  );
+}
+
+export default App;
